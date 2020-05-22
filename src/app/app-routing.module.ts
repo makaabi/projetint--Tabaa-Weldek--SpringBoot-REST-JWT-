57abeaaa30538527e31e35f304398b271f35a995
makaabi <maminekaabi@gmail.com>
2020-05-22 10:35:49 +0100

    version 0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './application/home/home.component';
+import { PagenotfoundComponent } from './application/pagenotfound/pagenotfound.component';
+import { LoginComponent } from './application/login/login.component';
+import { SignupComponent } from './application/signup/signup.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'login', component:LoginComponent},
+  {path:'signup', component:SignupComponent},
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  {path:'**', component:PagenotfoundComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './application/home/home.component';
+import { PagenotfoundComponent } from './application/pagenotfound/pagenotfound.component';
+import { LoginComponent } from './application/login/login.component';
+import { SignupComponent } from './application/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PagenotfoundComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
